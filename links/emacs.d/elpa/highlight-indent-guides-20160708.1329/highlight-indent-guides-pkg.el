6fd7592ada509ee954c7aafb4904bc690ca72a26
Stephen Ribich <stephen.ribich@gmail.com>
2016-10-27 12:24:49 -0400

    reorganize file structure

---
+++ links/emacs.d/elpa/highlight-indent-guides-20160708.1329/highlight-indent-guides-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "highlight-indent-guides" "20160708.1329" "Minor mode to highlight indentation" 'nil :url "https://github.com/DarthFennec/highlight-indent-guides")
